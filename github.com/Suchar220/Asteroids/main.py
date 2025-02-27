560c5ed0e13bce4d2e621a173494f18642b857dc
Suchar220 <alukowal@gmail.com>
2025-02-27 14:28:46 +0100

    "constants"

--- github.com/Suchar220/Asteroids/main.py
+++ github.com/Suchar220/Asteroids/main.py
@@ -1,5 +1,7 @@
 import pygame
-
-print ("Starting Asteroids!") 
+from constants import *
+def main():
+    print("Starting Asteroids!")
+print (f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
     main()
