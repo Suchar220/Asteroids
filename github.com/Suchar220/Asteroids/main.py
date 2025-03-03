c05652816195f937cfb8829fe8ca991f9b4a659c
Suchar220 <alukowal@gmail.com>
2025-03-03 18:20:30 +0100

    containers

--- github.com/Suchar220/Asteroids/main.py
+++ github.com/Suchar220/Asteroids/main.py
@@ -6,22 +6,29 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
+    
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
         print(clock.get_fps())  
     
 if __name__ == "__main__":
