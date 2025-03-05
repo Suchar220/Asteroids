29e98ebd9ddf2bf52f43096e7db80004db0b41de
Suchar220 <alukowal@gmail.com>
2025-03-05 17:24:11 +0100

    split

--- github.com/Suchar220/Asteroids/asteroid.py
+++ github.com/Suchar220/Asteroids/asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 import pygame
-
+from constants import ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_KINDS
+import random
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y,radius)
@@ -13,3 +14,26 @@ class Asteroid(CircleShape):
     def update(self, dt):
         # Override the update method to move the asteroid
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return 
+        random_angle = random.uniform(20, 50)
+
+        new_velocity1 = self.velocity.rotate(random_angle)
+        new_velocity2 = self.velocity.rotate(-random_angle)
+        
+        # Calculate new radius
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        # Create two new asteroids
+        # First asteroid
+        new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid1.velocity = new_velocity1 * 1.2
+        
+        # Second asteroid
+        new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid2.velocity = new_velocity2 * 1.2
+
+        
